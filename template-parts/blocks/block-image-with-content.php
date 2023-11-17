e75b0917f0576701b3617126541cc06c99877d64
alimirza8 <alimirza.dev@gmail.com>
2023-11-17 23:48:06 +0500

    contact form

--- template-parts/blocks/block-image-with-content.php
+++ template-parts/blocks/block-image-with-content.php
@@ -3,21 +3,37 @@ $classes = 'layout-' . get_query_var('layout_index');
 if (get_sub_field('section_classes')) {
   $classes .= ' ' . get_sub_field('section_classes');
 }
+if (get_sub_field('enable_button') == 'Yes'){
+  $classes .= ' ' . 'text--with-btn';
+}
 ?>
 <section <?php theme_section_id(); ?> class="<?php echo $classes; ?> text tcenter">
   <div class="container">
     <?php
-      theme_get_attachment( get_sub_field('image'), 'full', '', true );
+      if (get_sub_field('enable_image') == 'Yes'){
+        theme_get_attachment( get_sub_field('image'), 'full', '', true );
+      }
       theme_the_field( 'title', '<h2 class="section-title">', '</h2>', true );
       theme_the_field( 'subtitle', '<p class="section-description">', '</p>', true );
-      if ( get_sub_field( 'content' )){
     ?>
+    <?php if ( get_sub_field( 'content' )): ?>
       <div class="content">
         <?php theme_the_field( 'content', '', '', true ); ?>
       </div>
+    <?php endif; ?>
 
-    <?php
-      }
-    ?>
+    <?php if ( get_sub_field('enable_button') == 'Yes' ): ?>
+      <div class="actual-position">
+        <a href="#" class="btn button--sticky open-popup"><?php echo get_sub_field( 'button_text' ); ?></a>
+      </div>
+    <?php endif;?>
   </div>
 </section>
+
+<div class="popup" aria-hidden="true">
+    <div class="popup-bg"></div>
+    <div class="contact-form">
+      <!-- form here -->
+      <?php echo do_shortcode('[gravityform id="1" title="false"]'); ?>
+    </div>
+  </div>
